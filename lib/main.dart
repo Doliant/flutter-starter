d1e67e4ced5195c0e846f6c2332093f35e2073cf
xianyu <xianyu@nibirutech.com>
2019-08-28 16:46:42 +0800

    kill fish_redux

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_flipperkit/flutter_flipperkit.dart';
-import 'package:fish_redux/fish_redux.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 
-import 'pages/test/page.dart';
-import 'pages/test2/page.dart';
+import 'store/store.dart';
+
+import 'screens/screens.dart';
+import 'screens/index/screen.dart';
 
 void main() {
   FlipperClient flipperClient = FlipperClient.getDefault();
@@ -14,14 +16,14 @@ void main() {
     // 如果你使用 http 库, 你必须把它设置为 false 且使用 https://pub.dev/packages/flipperkit_http_interceptor
     // useHttpOverrides: false,
     // 可选， 用于过滤请求
-      filter: (HttpClientRequest request) {
-        String url = '${request.uri}';
-        if (url.startsWith('https://via.placeholder.com') ||
-            url.startsWith('https://gravatar.com')) {
-          return false;
-        }
-        return true;
-      }));
+    filter: (HttpClientRequest request) {
+      String url = '${request.uri}';
+      if (url.startsWith('https://via.placeholder.com') ||
+          url.startsWith('https://gravatar.com')) {
+        return false;
+      }
+      return true;
+    }));
   flipperClient.addPlugin(FlipperReduxInspectorPlugin());
   flipperClient.addPlugin(FlipperSharedPreferencesPlugin());
   flipperClient.start();
@@ -30,89 +32,17 @@ void main() {
 }
 
 Widget createApp() {
-  final AbstractRoutes routes = PageRoutes(
-    pages: <String, Page<Object, dynamic>>{
-      'test': TestPage(),
-      'test2': Test2Page(),
-    },
-  );
-
-  return MaterialApp(
-    title: "Test fish redux",
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData(
-      primaryColor: Colors.black,
-    ),
-    //通过routes.buildPage的方式生成一个Widget
-    home: routes.buildPage("test", null),
-    onGenerateRoute: (RouteSettings settings) {
-      return MaterialPageRoute<Object>(builder: (BuildContext context) {
-        return routes.buildPage(settings.name, settings.arguments);
-      });
-    },
-  );
-}
-
-
-class Starter extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-//    return Page(title: 'Starter');
-//    WidgetsApp MaterialApp CupertinoApp
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
+  return StoreProvider(
+    store: store,
+    child: MaterialApp(
       title: 'Starter',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Colors.black,
       ),
-      home: BasePage(title: 'Starter'),
-    );
-  }
-}
-
-class BasePage extends StatefulWidget {
-  final String title;
-
-  BasePage({this.title}) : super();
-
-  @override
-  HomePage createState() => HomePage();
-}
-
-class HomePage extends State<BasePage> {
-  void _onPress() {
-    print(35);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: renderBody(context),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _onPress,
-        tooltip: 'Increment',
-        backgroundColor: Colors.black,
-        child: Icon(Icons.adjust, color: Colors.white),
-      ),
-    );
-  }
-
-  Widget renderBody(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(color: Colors.white),
-      child: Center(
-        child: Text(
-          '...',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-            fontSize: 32,
-            color: Colors.black87,
-          ),
-        ),
-      ),
-    );
-  }
+      home: ScreenIndex(),
+//    initialRoute: Routes.index,
+      onGenerateRoute: Screens().onGenerate,
+    ),
+  );
 }
